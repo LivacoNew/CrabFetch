0d15a2ab3ce5c8f28479ef231b2a7693d6419ad3
Sam <livaco@livaco.dev>
2024-03-13 14:19:43 +0000

    Config + parsing

---
+++ src/config_manager.rs
@@ -0,0 +1,52 @@
+use config::Config;
+use serde::Deserialize;
+use std::env;
+
+#[derive(Deserialize)]
+pub struct Configuration {
+    pub enable_cpu: bool,
+    pub cpu_format: String
+}
+
+pub fn parse() -> Configuration {
+    // Find the config path
+    // Tries $XDG_CONFIG_HOME/CrabFetch before backing up to $HOME/.config/CrabFetch
+    let config_path_str: String = match env::var("XDG_CONFIG_HOME") {
+        Ok(mut r) => {
+            r.push_str("/CrabFetch/config.toml");
+            r
+        }
+        Err(_) => {
+            // Let's try the home directory
+            let mut home_dir: String = match env::var("HOME") {
+                Ok(r) => {
+                    r
+                },
+                Err(e) => {
+                    // why tf would you unset home lmao
+                    panic!("Unable to find config folder; {}", e);
+                }
+            };
+            home_dir.push_str("/.config/CrabFetch/config.toml");
+            home_dir
+        }
+    };
+    println!("{}", config_path_str);
+
+    let mut builder = Config::builder();
+    builder = builder.add_source(config::File::with_name(&config_path_str).required(false));
+    // Set the defaults here
+    builder = builder.set_default("enable_cpu", true).unwrap();
+    builder = builder.set_default("cpu_format", "penis").unwrap();
+    // Now stop.
+    let config = match builder.build() {
+        Ok(r) => r,
+        Err(e) => panic!("Unable to parse config.toml: {}", e),
+    };
+
+    let deserialized = match config.try_deserialize::<Configuration>() {
+        Ok(r) => r,
+        Err(e) => panic!("Unable to parse config.toml: {}", e),
+    };
+    return deserialized;
+}
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,13 @@
-use cpu::CPUInfo;
-use memory::MemoryInfo;
+use crate::{config_manager::Configuration, memory::MemoryInfo, cpu::CPUInfo};
 
 mod cpu;
 mod memory;
+mod config_manager;
 
 fn main() {
+    let config: Configuration = config_manager::parse();
+    println!("{}", config.enable_cpu);
+
     let cpu: CPUInfo = cpu::get_cpu();
     let memory: MemoryInfo = memory::get_memory();
 }

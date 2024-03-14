5218d37bc7737d4037ae75267510b00ec9e93cfe
Sam <livaco@livaco.dev>
2024-03-14 17:52:05 +0000

    Seperate titles and values

--- src/main.rs
+++ src/main.rs
@@ -15,9 +15,15 @@ fn main() {
     let memory: MemoryInfo = memory::get_memory();
 
     if config.enable_cpu {
-        println!("{}", cpu.format(&config.cpu_format));
+        let mut str = config.cpu_title;
+        str.push_str(&config.seperator);
+        str.push_str(&cpu.format(&config.cpu_format));
+        println!("{}", str);
     }
     if config.enable_memory {
-        println!("{}", memory.format(&config.memory_format));
+        let mut str = config.memory_title;
+        str.push_str(&config.seperator);
+        str.push_str(&memory.format(&config.memory_format));
+        println!("{}", str);
     }
 }

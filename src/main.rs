0b52a4ab6a9f0d480a0b1deb0a2e3ccfe9f1baa8
Sam <livaco@livaco.dev>
2024-03-15 12:40:46 +0000

    get the printing working as intended

--- src/main.rs
+++ src/main.rs
@@ -17,20 +17,30 @@ fn main() {
 
     let ascii = ascii::get_ascii("default");
 
-    println!("{}", ascii);
+    // let line_number: u8 = 0;
+    let target_length = 48;
+    for line in ascii.split("\n") {
+        print!("{}", line);
+        let remainder = target_length - line.len();
+        for _ in 0..remainder {
+            print!(" ");
+        }
 
-    if config.enable_cpu {
-        let mut str = String::new();
-        str.push_str(&config_manager::color_string(&config.cpu_title, &config.title_color).to_string());
-        str.push_str(&config.seperator);
-        str.push_str(&cpu.format(&config.cpu_format));
-        println!("{}", str);
-    }
-    if config.enable_memory {
-        let mut str = String::new();
-        str.push_str(&config_manager::color_string(&config.memory_title, &config.title_color).to_string());
-        str.push_str(&config.seperator);
-        str.push_str(&memory.format(&config.memory_format));
-        println!("{}", str);
+        if config.enable_cpu {
+            let mut str = String::new();
+            str.push_str(&config_manager::color_string(&config.cpu_title, &config.title_color).to_string());
+            str.push_str(&config.seperator);
+            str.push_str(&cpu.format(&config.cpu_format));
+            print!("{}", str);
+        }
+        if config.enable_memory {
+            let mut str = String::new();
+            str.push_str(&config_manager::color_string(&config.memory_title, &config.title_color).to_string());
+            str.push_str(&config.seperator);
+            str.push_str(&memory.format(&config.memory_format));
+            print!("{}", str);
+        }
+
+        println!();
     }
 }

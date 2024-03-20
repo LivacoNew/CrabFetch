621a254fdbbe9f618f4ddee4478714fd33011678
Sam <livaco@livaco.dev>
2024-03-20 10:06:50 +0000

    improve ascii

--- src/ascii.rs
+++ src/ascii.rs
@@ -8,23 +8,19 @@ pub fn get_ascii(os: &str) -> (String, u64) {
 
 // Define art down below here
 fn arch() -> (String, u64) {
-("                  .o+`
-                 `ooo/
-                `+oooo:
-               `+oooooo:
-               -+oooooo+:
-             `/:-:++oooo+:
-            `/++++/+++++++:
-           `/++++++++++++++:
-          `/+++ooooooooooooo/`
-         ./ooosssso++osssssso+`
-        .oossssso-````/ossssss+`
-       -osssssso.      :ssssssso.
-      :osssssss/        osssso+++.
-     /ossssssss/        +ssssooo/-
-   `/ossssso+/:-        -:/+osssso+-
-  `+sso+:-`                 `.-/+oso:
- `++:.                           `-/+/
- .`                                 `/"
- .to_string(), 40)
+("             ..
+             -=.
+            -==-
+           -====-
+          -======-
+         -========-
+        -==========-
+       -============-
+      -====-:  .-====-
+     -=====.     =====-
+    -=====-      -=====-.
+  .-====--:      :--====-.
+ .-=-:.              .:-==.
+.:.                      .:."
+ .to_string(), 28)
 }

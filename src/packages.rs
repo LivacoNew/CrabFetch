4879b6cd264d6b6a8d0659ebf323f3419c36fd98
Sam <livaco@livaco.dev>
2024-03-23 20:34:47 +0000

    add flatpak support

--- src/packages.rs
+++ src/packages.rs
@@ -15,6 +15,9 @@ impl Module for PackagesInfo {
     fn format(&self, format: &str, _: u32) -> String {
         let mut str: String = String::new();
         for (manager, count) in &self.packages {
+            if str.len() > 0 {
+                str.push_str(", ");
+            }
             str.push_str(&format.replace("{manager}", &manager)
                          .replace("{count}", &count.to_string()));
         }
@@ -35,6 +38,10 @@ pub fn get_packages() -> PackagesInfo {
         Some(r) => {packages.packages.insert("pacman".to_string(), r);},
         None => {}
     };
+    match process_flatpak_packages() {
+        Some(r) => {packages.packages.insert("flatpak".to_string(), r);},
+        None => {}
+    };
 
     if packages.packages.len() <= 0 {
         packages.packages.insert("No Package Managers Found".to_string(), 0);
@@ -43,10 +50,9 @@ pub fn get_packages() -> PackagesInfo {
     packages
 }
 
+// Credit for Pacman/Flatpak detection goes to FastFetch, they were big brain while I was running pacman -Q like a dummy
 fn process_pacman_packages() -> Option<u64> {
-    // Credit to FastFetch here, I was running pacman -Q like a dummy
-    // https://github.com/fastfetch-cli/fastfetch/blob/dev/src/detection/packages/packages_linux.c#L256
-    let pacman_path = Path::new("/var/lib/pacman/local");
+    let pacman_path: &Path = Path::new("/var/lib/pacman/local");
     if !pacman_path.exists() {
         return None
     }
@@ -60,4 +66,32 @@ fn process_pacman_packages() -> Option<u64> {
     };
 
     Some(dir.count() as u64)
+}
+fn process_flatpak_packages() -> Option<u64> {
+    // This counts everything in /app and /runtime
+    let flatpak_apps_path: &Path = Path::new("/var/lib/flatpak/app");
+    if !flatpak_apps_path.exists() {
+        return None
+    }
+    let flatpak_apps_dir: ReadDir = match read_dir(flatpak_apps_path) {
+        Ok(r) => r,
+        Err(_) => {
+            return None
+        },
+    };
+    let flatpak_apps: u64 = flatpak_apps_dir.count() as u64;
+
+    let flatpak_runtime_path: &Path = Path::new("/var/lib/flatpak/runtime");
+    if !flatpak_runtime_path.exists() {
+        return None
+    }
+    let flatpak_runtime_dir: ReadDir = match read_dir(flatpak_runtime_path) {
+        Ok(r) => r,
+        Err(_) => {
+            return None
+        },
+    };
+    let flatpak_runtime: u64 = flatpak_runtime_dir.count() as u64;
+
+    Some(flatpak_apps + flatpak_runtime)
 }

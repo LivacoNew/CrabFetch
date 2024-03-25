4921a55fdb66ecef67916efbf9b79bb35f3f7f50
Sam <livaco@livaco.dev>
2024-03-25 17:50:48 +0000

    add debian ascii

--- src/ascii.rs
+++ src/ascii.rs
@@ -12,6 +12,7 @@ pub fn get_ascii(os: &str) -> (String, u16) {
 
     match os {
         "arch" => arch(),
+        "debian" => debian(),
         _ => ("".to_string(), 0)
     }
 }
@@ -32,5 +33,24 @@ fn arch() -> (String, u16) {
   .-====--:      :--====-.
  .-=-:.              .:-==.
 .:.                      .:."
+ .to_string(), 28)
+}
+fn debian() -> (String, u16) {
+("         .^!7!~~~^:
+      :!JPPP55YY5555Y?!:
+    ^YPG5?!:.    .:~?PGPJ:
+   7G57^.            .7PG5~
+ .YP7.       :^^^:.    !PY^
+ JG7       .!~:....     YP:
+.PY       .?.       .   YP^
+.P?       :Y       .   :P7
+.P?       .??.   .   .~Y!
+ JP.      ..~?7~^:^~77!.
+ ^PY~        .^^~~^:.
+  ~PP^
+   :YP^
+     !Y?^
+       ~??~^.
+         .^~~:..            "
  .to_string(), 28)
 }

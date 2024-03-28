b755e466d108933571e9af21bd5df28ae460079a
Sam <livaco@livaco.dev>
2024-03-28 14:48:17 +0000

    Attempt to fix konsole being broken

--- src/terminal.rs
+++ src/terminal.rs
@@ -33,8 +33,10 @@ pub fn get_terminal() -> TerminalInfo {
     // Essentially, it grabs the parent process, grabs the parent of that pid from /proc/x/stat and
     // gets the name of it from ps
 
+    // println!("Starting terminal:");
     let parent_pid: u32 = process::parent_id();
     let path: String = format!("/proc/{}/stat", parent_pid);
+    // println!("Shell proccess ID should be {} leading to {}", parent_pid, path);
 
     let mut parent_stat: File = match File::open(path.to_string()) {
         Ok(r) => r,
@@ -51,6 +53,7 @@ pub fn get_terminal() -> TerminalInfo {
             return terminal
         },
     }
+    // println!("Got contents: {}", contents);
 
     let terminal_pid: u32 = match contents.split(" ").collect::<Vec<&str>>()[3].parse() {
         Ok(r) => r,
@@ -64,6 +67,7 @@ pub fn get_terminal() -> TerminalInfo {
     // sysinfo were too slow
     // println!("{}", terminal_pid);
     let path: String = format!("/proc/{}/cmdline", terminal_pid);
+    // println!("Got terminal process ID as {} leading to {}", terminal_pid, path);
 
     let mut terminal_cmdline: File = match File::open(path.to_string()) {
         Ok(r) => r,
@@ -80,9 +84,13 @@ pub fn get_terminal() -> TerminalInfo {
             return terminal
         },
     }
-
+    // println!("Got full cmdline as {}", contents);
     contents = contents.split(" ").collect::<Vec<&str>>()[0].to_string();
+    // Fix for this happening; https://cdn.discordapp.com/attachments/1011301373482115163/1221945908250280096/image.png?ex=66146ccf&is=6601f7cf&hm=2045e0d8150ff468c84ee0fe10ca9105dd4793df05c599715bd1bd7c74d4dc9d&
+    contents = contents.split("--").next().unwrap().to_string();
+    // println!("Filter 1: {}", contents);
     contents = contents.split("/").last().unwrap().to_string();
+    // println!("Filter 2: {}", contents);
     terminal.terminal_name = contents;
 
     terminal

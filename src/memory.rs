f08ea89f676ed92ebd0581c72c767a2f077316de
Sam <livaco@livaco.dev>
2024-04-21 19:35:27 +0100

    parse meminfo properly

--- src/memory.rs
+++ src/memory.rs
@@ -61,10 +61,10 @@ impl Module for MemoryInfo {
 
         CONFIG.memory.format.replace("{phys_used_kib}", &MemoryInfo::round(self.used_kib as f32, dec_places).to_string())
             .replace("{phys_used_mib}", &MemoryInfo::round(self.used_kib as f32 / 1024.0, dec_places).to_string())
-            .replace("{phys_used_gib}", &MemoryInfo::round(self.used_kib as f32 / 104857.0, dec_places).to_string())
+            .replace("{phys_used_gib}", &MemoryInfo::round(self.used_kib as f32 / 1.049e+5, dec_places).to_string())
             .replace("{phys_max_kib}", &MemoryInfo::round(self.max_kib as f32, dec_places).to_string())
             .replace("{phys_max_mib}", &MemoryInfo::round(self.max_kib as f32 / 1024.0, dec_places).to_string())
-            .replace("{phys_max_gib}", &MemoryInfo::round(self.max_kib as f32 / 104857.0, dec_places).to_string())
+            .replace("{phys_max_gib}", &MemoryInfo::round(self.max_kib as f32 / 1.049e+5, dec_places).to_string())
             .replace("{percent}", &MemoryInfo::round(self.percentage, dec_places).to_string())
     }
 }
@@ -95,11 +95,7 @@ pub fn get_memory() -> MemoryInfo {
 
     // MemUsed = Memtotal + Shmem - MemFree - Buffers - Cached - SReclaimable
     // From https://github.com/dylanaraps/neofetch/blob/master/neofetch#L2676
-    let mut shmem: u32 = 0;
-    let mut mem_free: u32 = 0;
-    let mut buffers: u32 = 0;
-    let mut cached: u32 = 0;
-    let mut s_reclaimable: u32 = 0;
+    let mut mem_available: u32 = 0;
 
     for line in lines {
         if line.starts_with("MemTotal") {
@@ -113,65 +109,20 @@ pub fn get_memory() -> MemoryInfo {
                 }
             }
         }
-        if line.starts_with("MemFree") {
+        if line.starts_with("MemAvailable") {
             let mut var: &str = line.split(": ").collect::<Vec<&str>>()[1];
             var = &var[..var.len() - 4].trim();
-            mem_free = match var.to_string().parse::<u32>() {
+            mem_available = match var.to_string().parse::<u32>() {
                 Ok(r) => r,
                 Err(e) => {
-                    log_error("Memory", format!("Could not parse free memory: {}", e));
-                    0
-                }
-            }
-        }
-        if line.starts_with("Shmem:") {
-            let mut var: &str = line.split(": ").collect::<Vec<&str>>()[1];
-            var = &var[..var.len() - 4].trim();
-            shmem = match var.to_string().parse::<u32>() {
-                Ok(r) => r,
-                Err(e) => {
-                    log_error("Memory", format!("Could not parse memory shmem: {}", e));
-                    0
-                }
-            }
-        }
-        if line.starts_with("Buffers") {
-            let mut var: &str = line.split(": ").collect::<Vec<&str>>()[1];
-            var = &var[..var.len() - 4].trim();
-            buffers = match var.to_string().parse::<u32>() {
-                Ok(r) => r,
-                Err(e) => {
-                    log_error("Memory", format!("Could not parse memory buffers: {}", e));
-                    0
-                }
-            }
-        }
-        if line.starts_with("Cached") {
-            let mut var: &str = line.split(": ").collect::<Vec<&str>>()[1];
-            var = &var[..var.len() - 4].trim();
-            cached = match var.to_string().parse::<u32>() {
-                Ok(r) => r,
-                Err(e) => {
-                    log_error("Memory", format!("Could not parse cached memory: {}", e));
-                    0
-                }
-            }
-        }
-        if line.starts_with("SReclaimable") {
-            let mut var: &str = line.split(": ").collect::<Vec<&str>>()[1];
-            var = &var[..var.len() - 4].trim();
-            s_reclaimable = match var.to_string().parse::<u32>() {
-                Ok(r) => r,
-                Err(e) => {
-                    log_error("Memory", format!("Could not parse sreclaimable memory: {}", e));
+                    log_error("Memory", format!("Could not parse memfree memory: {}", e));
                     0
                 }
             }
         }
     }
 
-    // MemUsed = Memtotal + Shmem - MemFree - Buffers - Cached - SReclaimable
-    memory.used_kib = memory.max_kib + shmem - mem_free - buffers - cached - s_reclaimable;
+    memory.used_kib = memory.max_kib - mem_available;
     memory.percentage = (memory.used_kib as f32 / memory.max_kib as f32) * 100.0;
 
     memory

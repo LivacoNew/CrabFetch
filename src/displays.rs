69defea0a2354c98cb56dcd766db086fa56acd1c
Sam <livaco@livaco.dev>
2024-06-24 18:09:37 +0100

    add wayland for display info as well

--- src/displays.rs
+++ src/displays.rs
@@ -1,7 +1,8 @@
 use core::str;
-use std::env;
+use std::{collections::HashMap, env};
 
 use serde::Deserialize;
+use wayland_client::{protocol::{wl_output, wl_registry}, ConnectError, Connection, Dispatch, QueueHandle};
 use x11rb::{connection::RequestConnection, protocol::{randr::{self, MonitorInfo}, xproto::{ConnectionExt, CreateWindowAux, Screen, Window, WindowClass}}, COPY_DEPTH_FROM_PARENT};
 
 use crate::{config_manager::{Configuration, CrabFetchColor}, Module, ModuleError};
@@ -77,13 +78,14 @@ pub fn get_displays() -> Result<Vec<DisplayInfo>, ModuleError> {
     // Instead of relying on XDG_SESSION_TYPE line Desktop, I simply just check the sockets as it
     // can report any string and break if someone's dumb enough to do that
     if env::var("WAYLAND_DISPLAY").is_ok() {
-        todo!();
+        return fetch_wayland();
     } else if env::var("DISPLAY").is_ok() {
         return fetch_xorg();
     } else {
         return Err(ModuleError::new("Display", format!("Could not identify desktop session type.")));
     }
 }
+
 
 fn fetch_xorg() -> Result<Vec<DisplayInfo>, ModuleError> {
     // This has really opened my eyes as to why more pieces of software haven't swapped over to
@@ -133,4 +135,102 @@ fn fetch_xorg() -> Result<Vec<DisplayInfo>, ModuleError> {
     // Not error checked as it's not a huge problem if this fails, moreso just a "would be nice to do" thing
     let _ = conn.destroy_window(win_id);
     Ok(displays)
+}
+
+
+
+//
+// The Wayland Zone
+//
+struct WaylandState {
+    complete: bool, // Tells us whether to break out of the event loop or not
+    num_outputs: u8, // How many outputs are waiting to be processed 
+    outputs: HashMap<wl_output::WlOutput, DisplayInfo> // The output data as it stands
+}
+impl Dispatch<wl_registry::WlRegistry, ()> for WaylandState {
+    fn event(state: &mut Self, reg: &wl_registry::WlRegistry, event: wl_registry::Event, _: &(), _: &Connection, qh: &QueueHandle<WaylandState>,) {
+        if let wl_registry::Event::Global {name, interface, version} = event {
+            if interface == "wl_output" {
+                // This is what we're looking for, bind to it
+                reg.bind::<wl_output::WlOutput, _, _>(name, version, &qh, ());
+                state.num_outputs += 1;   
+            }
+        }
+    }
+}
+impl Dispatch<wl_output::WlOutput, ()> for WaylandState {
+    fn event(state: &mut Self, output: &wl_output::WlOutput, event: wl_output::Event, _: &(), _: &Connection, _qh: &QueueHandle<WaylandState>,) {
+        if !state.outputs.contains_key(output) {
+            state.outputs.insert(output.clone(), DisplayInfo::new());
+        }
+
+        let display: &mut DisplayInfo = state.outputs.get_mut(output).unwrap();
+        if let wl_output::Event::Name {name} = &event {
+            display.name = name.to_string();
+        }
+        if let wl_output::Event::Mode { width, height, refresh, .. } = &event {
+            display.width = width.to_string().parse::<u16>().unwrap();
+            display.height = height.to_string().parse::<u16>().unwrap();
+            display.refresh_rate = match (*refresh as f32 / 1000.0).round().to_string().parse::<u16>() {
+                Ok(r) => Some(r),
+                // There's no real "error handling" here so just set it to 0 so it's not None and letting us get stuck in an infinite loop
+                Err(_) => Some(0)
+            };
+        }
+
+        if !display.name.is_empty() && display.width != 0 && display.height != 0 && display.refresh_rate.is_some() {
+            // We're done, release it 
+            output.release();
+            if state.num_outputs == 0 {
+                state.complete = true;
+                return;
+            }
+            state.num_outputs -= 1;
+        }
+    }
+}
+fn fetch_wayland() -> Result<Vec<DisplayInfo>, ModuleError> {
+    let conn: Connection = match Connection::connect_to_env() {
+        Ok(r) => r,
+        Err(e) => {
+            let msg: &str = match e {
+                ConnectError::NoWaylandLib => "Unable to load the Wayland library.",
+                ConnectError::NoCompositor => "Unable to find a Wayland compositor.",
+                ConnectError::InvalidFd => "Found a Wayland compositor, but the socket contained garbage."
+            };
+            return Err(ModuleError::new("Display", format!("Failed to connect to Wayland compositor: {}", msg)));
+        },
+    };
+    let display = conn.display();
+
+    let mut event_queue = conn.new_event_queue();
+    let qh = event_queue.handle();
+
+    let _registry = display.get_registry(&qh, ());
+    let mut data: WaylandState = WaylandState {
+        complete: false,
+        num_outputs: 0,
+        outputs: HashMap::new()
+    };
+
+    let mut loops: u16 = 0;
+    while !data.complete {
+        match event_queue.roundtrip(&mut data) {
+            Ok(r) => r,
+            Err(e) => {
+                return Err(ModuleError::new("Display", format!("Compositor roundtrip returned error: {}", e)));
+            }
+        };
+        loops += 1;
+        if loops > 1000 {
+            return Err(ModuleError::new("Display", "Wayland compositor took too long to respond; over 1000 event loops have passed.".to_string()));
+        }
+    }
+
+    // TODO: Clean this lol
+    let mut y: Vec<DisplayInfo> = Vec::new();
+    for x in data.outputs {
+        y.push(x.1.clone());
+    }
+    Ok(y)
 }

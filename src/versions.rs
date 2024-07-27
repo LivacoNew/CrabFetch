4ae2b79333633f001f8513d5776a64d7daf2d426
Sam <livaco@livaco.dev>
2024-07-27 16:40:44 +0100

    add some more hashes + sources for each of the current ones

--- src/versions.rs
+++ src/versions.rs
@@ -231,19 +231,23 @@ fn substitite_package_name(name: &str) -> &str {
 fn compare_hash(hash: &str) -> Option<String> {
     match hash {
         // Kitty
-        "bfc1a826895089928bd40eb09a340c6f3b6eb22d51589ca32c032761ff44843b" => Some("0.35.2".to_string()),
+        "bfc1a826895089928bd40eb09a340c6f3b6eb22d51589ca32c032761ff44843b" => Some("0.35.2".to_string()), // Arch
 
         // Alacritty
-        "da793f342a25fd9cb017154bc960d7e6a93d1d9d87a5bfdaf46738d7356fce13" => Some("0.13.2".to_string()),
+        "da793f342a25fd9cb017154bc960d7e6a93d1d9d87a5bfdaf46738d7356fce13" => Some("0.13.2".to_string()), // Arch
 
         // Foot
-        "2806412806ca7289f0f9fe1d73cd28c050f53204e46d9d5610acb0bac9f347ff" => Some("1.17.2".to_string()),
+        "2806412806ca7289f0f9fe1d73cd28c050f53204e46d9d5610acb0bac9f347ff" => Some("1.17.2".to_string()), // Arch 
 
         // Terminator
-        "8735bed0720a0f5ed8297fcf870a091199044a762dd360d439cfbb6b9871a7b1" => Some("2.1.4".to_string()),
+        "8735bed0720a0f5ed8297fcf870a091199044a762dd360d439cfbb6b9871a7b1" => Some("2.1.4".to_string()), // Arch
 
         // ZSH
-        "7ac8cc89b75b595955ec56d8e4b6047c2fc233a6a10c81a137c8417d17a9a970" => Some("5.9".to_string()),
+        "7ac8cc89b75b595955ec56d8e4b6047c2fc233a6a10c81a137c8417d17a9a970" => Some("5.9".to_string()), // Arch
+
+        // Bash
+        "c3a57cc13505b15535662f892fa73f2fd922d1de80be4fa6a4b78be8a59e69f8" => Some("5.2.26".to_string()), // Arch
+        "0936c178ac1e145ede22b277f8cb6a6ce3d1390492628ef999b941a65e51fe8e" => Some("5.2.21".to_string()), // VoidLinux live boot
 
         _ => None
     }

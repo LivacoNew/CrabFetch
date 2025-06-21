0daace05cf10b69113a1c6fa991e1be64f74b40b
Sam <livaco@livaco.dev>
2025-06-21 09:44:56 +0100

    chore: undo formatting that didn't match the rest of the code

--- src/ascii.rs
+++ src/ascii.rs
@@ -1,13 +1,9 @@
 use std::{borrow::Cow, cmp::min};
 
-use colored::{ColoredString, Colorize};
+use colored::Colorize;
 use serde::Deserialize;
 
-use crate::{
-    ascii_art,
-    config_manager::{self, Configuration},
-    formatter::CrabFetchColor,
-};
+use crate::{ascii_art, config_manager::{self, Configuration}, formatter::CrabFetchColor};
 
 #[derive(Deserialize)]
 pub struct AsciiConfiguration {
@@ -20,14 +16,14 @@ pub struct AsciiConfiguration {
     // Done this way because I kid you not I could not find a way to make an multi-type thing for
     // the Config crate
     pub solid_color: CrabFetchColor,
-    pub band_colors: Vec<CrabFetchColor>,
+    pub band_colors: Vec<CrabFetchColor>
 }
 #[derive(Debug, Deserialize, PartialEq)]
 pub enum AsciiMode {
     Raw,
     OS,
     Solid,
-    Band,
+    Band
 }
 
 // Return type is the ascii & the maximum length of it
@@ -40,7 +36,7 @@ pub fn find_ascii(os: &str, ignore_custom: bool) -> (Cow<'_, str>, u16) {
                 let stripped = strip_ansi_escapes::strip_str(x);
                 let len: usize = stripped.chars().count();
                 if len > length as usize {
-                    length = u16::try_from(len).expect("Unable to convert length to u16")
+                    length = u16::try_from(len).expect("Unable to convert length to u16");
                 }
             });
             return (Cow::Owned(user_override), length);
@@ -74,12 +70,7 @@ pub fn find_ascii(os: &str, ignore_custom: bool) -> (Cow<'_, str>, u16) {
     (Cow::Borrowed(art), max_len)
 }
 
-pub fn get_ascii_line(
-    current_line: usize,
-    ascii_split: &[&str],
-    target_length: u16,
-    config: &Configuration,
-) -> String {
+pub fn get_ascii_line(current_line: usize, ascii_split: &[&str], target_length: u16, config: &Configuration) -> String {
     let mut line: String = String::new();
 
     if ascii_split.len() > current_line {
@@ -95,7 +86,7 @@ pub fn get_ascii_line(
         line = match config.ascii.mode {
             AsciiMode::Raw => line,
             AsciiMode::OS | AsciiMode::Solid => color_solid(&line, config), // main func sets the solid color to be the os color in the OS's case
-            AsciiMode::Band => color_band_vertical(&line, current_line, ascii_split.len(), config),
+            AsciiMode::Band => color_band_vertical(&line, current_line, ascii_split.len(), config)
         }
     }
 
@@ -110,29 +101,16 @@ fn color_solid(line: &str, config: &Configuration) -> String {
     config.ascii.solid_color.color_string(line).to_string()
 }
 
-#[allow(
-    clippy::cast_precision_loss,
-    clippy::cast_possible_truncation,
-    clippy::cast_sign_loss
-)]
-fn color_band_vertical(
-    line: &str,
-    current_line: usize,
-    ascii_length: usize,
-    config: &Configuration,
-) -> String {
+#[allow(clippy::cast_precision_loss, clippy::cast_possible_truncation, clippy::cast_sign_loss)]
+fn color_band_vertical(line: &str, current_line: usize, ascii_length: usize, config: &Configuration) -> String {
     let percentage: f32 = current_line as f32 / (ascii_length - 1) as f32;
     let total_colors: usize = config.ascii.band_colors.len();
-    let index: usize = min(
-        (((total_colors - 1) as f32) * percentage).round() as usize,
-        total_colors - 1,
-    );
+    let index: usize = min((((total_colors - 1) as f32) * percentage).round() as usize, total_colors - 1);
 
-    let colored: ColoredString = config
-        .ascii
+    config.ascii
         .band_colors
         .get(index as usize)
         .unwrap()
-        .color_string(line);
-    colored.to_string()
+        .color_string(line)
+        .to_string()
 }
